343dbfd04214bec98be4f3891a9e15b7f4bd7afd
amida <amida@Elessar>
2022-10-29 11:22:12 -0600

    SQL script inserted and models completed

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -11,4 +11,28 @@ package models;
  */
 public class Role {
     
+    int roleID;
+    String roleName;
+    
+    public Role(){
+        
+    }
+    
+    public Role(int roleID, String roleName) {
+        this.roleID = roleID;
+        this.roleName = roleName;
+    }
+    
+    public void setRoleID(int roleID){
+        this.roleID = roleID;
+    }
+    
+    public int getRoleID() {
+        return roleID;
+    }
+    
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+    
 }
